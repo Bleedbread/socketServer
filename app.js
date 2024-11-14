a625134e8e7c06b7a90478ee454af06c52e459e2
Bleedbread <seng.bin42@gmail.com>
2024-11-14 10:06:13 +0900

    modified:   .env 	modified:   app.js 	modified:   index.js

--- app.js
+++ app.js
@@ -10,4 +10,25 @@ mongoose.connect(process.env.DB)
   .then(() => console.log("Connected to database"))
   .catch((err) => console.error("Database connection error:", err));
 
+  
+//  임의로 룸을 만들어주기
+app.get("/", async (req, res) => {
+  Room.insertMany([
+    {
+      room: "자바스크립트 단톡방",
+      members: [],
+    },
+    {
+      room: "리액트 단톡방",
+      members: [],
+    },
+    {
+      room: "NodeJS 단톡방",
+      members: [],
+    },
+  ])
+    .then(() => res.send("ok"))
+    .catch((error) => res.send(error));
+});
+
 module.exports = app;
